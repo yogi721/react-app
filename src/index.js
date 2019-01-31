65c6d239aa1bf35d2b3ba2a7f24e18093846e9c6
yogi721 <semlali.mehdi.1980@gmail.com>
2019-01-31 02:29:39 +0100

    add new component and use it in index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import Couter from './components/counter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Couter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, 
 // you can change
